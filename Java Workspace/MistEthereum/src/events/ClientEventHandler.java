40d2dcd7155a033bcc898a3edbc8baa5421d3a43
minta39 <radiomix@hotmail.se>
2017-03-01 15:24:05 +0100

    small fix

--- Java Workspace/MistEthereum/src/events/ClientEventHandler.java
+++ Java Workspace/MistEthereum/src/events/ClientEventHandler.java
@@ -72,10 +72,10 @@ public class ClientEventHandler extends EventHandler{
 	private void ForwardData(String[] s)
 	{
 		s[2] = ((UserState)Owner.GetState()).Address;
-//		if(s[2].equals("null"))
-//		{
-//			return;
-//		}
+		if(s[2].equals("null"))
+		{
+			return;
+		}
 		String mm = s[0] + "," + s[1] + "," + s[2] + "," + s[3] + "," + s[4];
 		SendToMiner(mm);
 	}
