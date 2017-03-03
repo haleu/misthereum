a40b5e37714f100e0a6da5deb48d55b2c7664f1a
henpet <henrikpettersson92@gmail.com>
2017-03-03 14:05:22 +0100

    asdf

--- Java Workspace/MistEthereum/src/server/data/UserState.java
+++ Java Workspace/MistEthereum/src/server/data/UserState.java
@@ -5,7 +5,7 @@ public class UserState extends ClientState{
 	private static int UserID = 0;
 	public int ID = 0;
 	
-	public String Address = "0xa45d59b32510907d0ba3d7ced0a40274e7a9bfaa";
+	public String Address = "";
 	public String Username;
 	public String Password;
 	
